1c69816b8c2495ab8099615661cc180a10b71092
Natallia Vashkevich <mahatali84@gmail.com>
2025-01-02 21:49:01 +0300

    refactor: refactor 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = process;
 const filePath = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(filePath);
-readStream.pipe(process.stdout);
+readStream.on("data", (text) => stdout.write(text));
