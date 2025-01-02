d3ba57748e62deba3686a16bde7dd6a693b44a63
Natallia Vashkevich <mahatali84@gmail.com>
2025-01-02 22:46:12 +0300

    feat: implement 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+const { stdin, stdout } = process;
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+
+stdout.write('Hi! Would you like to write some words about yourself?\n If you want to exit, write "exit"\n');
+
+const input = readline.Interface(stdin);
+
+input.on('line', (data) => {
+  if(data === 'exit') {
+    process.exit();
+  } 
+  writeStream.write(data);
+});
+process.on('exit', () => stdout.write('Bye!'));
